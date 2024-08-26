8edd55c74b02288a62f7d38f074341aaa905ef5c
Dziyana Drahun <Dziyana.Drahun@servicechannel.com>
2024-08-27 00:56:07 +0300

    Day 4. Working with the if/else construct

--- day_4/Day_4_Heads_or_Tails.py
+++ day_4/Day_4_Heads_or_Tails.py
@@ -1,8 +1,16 @@
 import random
-
 test_random_function=random.randint(0, 1)
 print(test_random_function)
 if test_random_function==0:
     print("Heads")
 else:
     print("Tails")
+
+#2
+import random
+test_random_float_function=random.uniform(0, 2)
+print(test_random_float_function)
+if test_random_float_function>=1:
+    print("Tails")
+else:
+    print("Heads")    
