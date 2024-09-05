1dc58ad4c5fea6f89590dbc2e24083549182f820
Dziyana Drahun <Dziyana.Drahun@servicechannel.com>
2024-09-05 08:10:24 +0300

    Add new logic. Create a list of random integer numbers from 1 to 1000 of size 500.

--- day_5/Day_5_Highest_Score.py
+++ day_5/Day_5_Highest_Score.py
@@ -10,3 +10,21 @@ for number in list_of_numbers:
     if number > max_number:
         max_number = number
 print(max_number)
+
+#Task 2
+from random import randint
+big_list = []
+for i in range(500):
+    big_list.append(randint(1,1000))
+print(big_list)
+
+sum_big_list = 0
+for number in big_list:
+    sum_big_list = sum_big_list + number
+print(sum_big_list)
+
+max_big_list = 0
+for number in big_list:
+    if number > max_big_list:
+        max_big_list = number
+print(max_big_list)
