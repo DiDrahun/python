1ad9a338aa3ecde686833f5c7f19d1f3a0ff2fa9
DiDrahun <106480560+DiDrahun@users.noreply.github.com>
2025-02-21 17:14:30 +0300

    Update Day_5_Final_Project_Password_Generator.py
    implemented shuffle

--- day_5/Day_5_Final_Project_Password_Generator.py
+++ day_5/Day_5_Final_Project_Password_Generator.py
@@ -1,4 +1,3 @@
-#Password Generator Project
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -11,20 +10,17 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-easy_password = ""
-for character in range (0, nr_letters):
-    easy_password += random.choice(letters) 
-for character in range (0, nr_numbers):
-    easy_password += random.choice(numbers) 
-for character in range (0, nr_symbols):
-    easy_password += random.choice(symbols) 
-print(easy_password)
-
-my_password = list(easy_password)
-random.shuffle(my_password)
-strong_password = ""
-for i in my_password:
-    strong_password += str(i) 
-print(strong_password)
-
-
+password = ""
+while nr_letters  + nr_symbols + nr_numbers != 0:
+    selecter = random.randint(1,3)
+    if selecter ==1 and nr_letters > 0:
+        password = password + random.choice(letters)
+        nr_letters = nr_letters - 1
+    elif selecter == 2 and nr_numbers > 0:
+        password += random.choice(numbers)
+        nr_numbers = nr_numbers -1
+    elif selecter == 3 and nr_symbols > 0:
+        password += random.choice(symbols)
+        nr_symbols = nr_symbols -1
+        
+print(password)
